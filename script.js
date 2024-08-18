e972b01f6a1e0918f16360dbed8d9e7bbf0b6fcd
varshithchowdary <145253785+varshithchowdary@users.noreply.github.com>
2024-08-18 11:59:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_KEY = "1d3a0eefa97b499d8fbc4ee93eeb40b7";
+const API_KEY = "77fee5a498124aa1810d4cec152b0563";
 const url = "https://newsapi.org/v2/everything?q=";
 
 window.addEventListener("load", () => fetchNews("India"));
